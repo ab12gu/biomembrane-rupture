deb26554630616c4487cdba1b9ea100c8a52e7be
ab12gu <ab18gu@gmail.com>
2018-04-22 19:55:59 -0700

    Reset Files

--- indxx.m
+++
@@ -1,6 +0,0 @@
-function [index] = indxx(i,j,rowLength)
-%Solve for the index
-index = (i-1)*rowLength+j;
-
-end
-
--- pinForce.m
+++
@@ -1,8 +0,0 @@
-function [output] = pinForce(radDiff, maxRadius, angDiff, force)
-%Function Name: pinForce
-%   Function that defines the force caused by pins near center
-
-output = (1-radDiff/maxRadius)*cos(angDiff)+force;
-
-end
-
